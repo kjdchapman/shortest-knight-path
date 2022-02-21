b293b018e95c20379a35f650588ba64b988d961f
kjdchapman <kjdchapman@gmail.com>
2022-02-21 13:25:22 +0000

    Re-order specs so move counts are together

--- spec/knight_spec.rb
+++ spec/knight_spec.rb
@@ -3,23 +3,29 @@ require_relative "../knight"
 describe 'knight movement' do
   subject { CountKnightMovement.new }
 
+  it 'takes zero moves from a3 to a3' do
+    expect(subject.execute('a3', 'a3')).to eq(0)
+  end
+
   it 'takes one move from a3 to b5' do
     expect(subject.execute('a3', 'b5')).to eq(1)
   end
 
-  it 'takes zero moves from a3 to a3' do
-    expect(subject.execute('a3', 'a3')).to eq(0)
+  it 'takes one move from f5 to d6' do
+    expect(subject.execute('f5','d6')).to eq(1)
+  end
+
+  it 'takes one move from f5 to e3' do
+    expect(subject.execute('f5', 'e3')).to eq(1)
   end
 
   it 'takes two moves from f5 to d5' do
     expect(subject.execute('f5', 'd5')).to eq(2)
   end
 
-  it 'takes two moves from f5 to e3' do
-    expect(subject.execute('f5', 'e3')).to eq(1)
-  end
-
   it 'takes two moves from f5 to e6' do
     expect(subject.execute('f5', 'e6')).to eq(2)
   end
+
+
 end
