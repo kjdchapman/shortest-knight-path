c9a49e80eea67cbd6f79fe5070e7934878f80020
kjdchapman <kjdchapman@gmail.com>
2022-03-16 13:29:47 +0000

    Extract method around single position

--- knight.rb
+++ knight.rb
@@ -5,7 +5,7 @@ class CountKnightMovement
     number_of_moves = 0
 
     if first_position == "f5"
-      if ["e3","d6", "e7"].any?(last_position)
+      if one_move_away_from_f5?(last_position)
         number_of_moves = 1
       else
         number_of_moves = 2
@@ -19,6 +19,14 @@ class CountKnightMovement
       end
     end
 
+    if first_position == "e5"
+      number_of_moves = 1
+    end
+
     number_of_moves
+  end
+
+  def one_move_away_from_f5?(last_position)
+    ["e3", "d6", "e7"].any?(last_position)
   end
 end
